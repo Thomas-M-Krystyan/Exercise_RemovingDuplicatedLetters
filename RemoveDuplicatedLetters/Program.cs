ff32a0b2deb25f2c117bb72a2aabea549f5120ca
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2017-10-20 02:52:30 +0200

    Upload files from hard drive

---
+++ RemoveDuplicatedLetters/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace RemoveDuplicatedLetters
+{
+    internal static class Program
+    {
+        internal static void Main(string[] args)
+        {
+            Console.WriteLine("Remove duplicated letters\n(List algorithm):\n");
+            Console.WriteLine(StringAnalyzer.ReplaceDuplicatedLetters1("Ananas"));
+            Console.WriteLine(StringAnalyzer.ReplaceDuplicatedLetters1("Banana"));
+            Console.WriteLine(StringAnalyzer.ReplaceDuplicatedLetters1("Coconut"));
+
+            Console.WriteLine("\n" + new string('-', 30));
+
+            Console.WriteLine("\nRemove duplicated letters\n(HashSet algorithm):\n");
+            Console.WriteLine(StringAnalyzer.ReplaceDuplicatedLetters2("Ananas"));
+            Console.WriteLine(StringAnalyzer.ReplaceDuplicatedLetters2("Banana"));
+            Console.WriteLine(StringAnalyzer.ReplaceDuplicatedLetters2("Coconut"));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RemoveDuplicatedLetters/StringAnalyzer.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace RemoveDuplicatedLetters
+{
+    internal static class StringAnalyzer
+    {
+    // Solution with handmade implementation of HashSet collection and StringBuilder
+    #region First algorithm
+        internal static string ReplaceDuplicatedLetters1(string textData)
+        {
+            // List of skipped (already used) letters
+            List<char> usedLetters = new List<char>();
+
+            // New string without duplicated letters
+            StringBuilder stringBuilder = new StringBuilder();
+
+            foreach (char letter in textData.ToLower())
+            {
+                if (usedLetters.Contains(letter))
+                {
+                    continue;
+                }
+                stringBuilder.Append(letter);
+                usedLetters.Add(letter);
+            }
+
+            return stringBuilder.ToString();
+        }
+    #endregion
+
+    // Faster solution with using already built-in HashSet collection and concatenation
+    #region Second algorithm
+        internal static string ReplaceDuplicatedLetters2(string textData)
+        {
+            // Create unique collection of letters
+            HashSet<char> uniqueLetters = new HashSet<char>();
+            foreach (char letter in textData.ToLower())
+            {
+                uniqueLetters.Add(letter);
+            }
+
+            // Convert content of HashSet into string
+            return string.Join("", uniqueLetters);
+        }
+    #endregion
+    }
+}
